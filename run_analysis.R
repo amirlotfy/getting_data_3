ecbed0a2034ea2646b8676c1aafa3ded803a3b32
amirlotfy <amir.lotfy@outlook.com>
2019-10-01 12:51:21 -0400

    Test

---
+++ run_analysis.R
@@ -0,0 +1,8 @@
+setwd("C:/Users/Amir Lotfy/R_working_directory/UCI HAR Dataset/")
+
+subject_train <- read.table("train/subject_train.txt")
+subject_test <- read.table("test/subject_test.txt")
+X_train <- read.table("train/X_train.txt")
+X_test <- read.table("test/X_test.txt")
+y_train <- read.table("train/y_train.txt")
+y_test <- read.table("test/y_test.txt")
