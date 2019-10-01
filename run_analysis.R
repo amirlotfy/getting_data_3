f3f6977cba4713a89677ec465bfbf08e32b45850
amirlotfy <amir.lotfy@outlook.com>
2019-10-01 14:21:14 -0400

    Not sure if the melted dataframe is correct or not. need someone to cross check

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,7 @@
+#set working Directory 
 setwd("C:/Users/Amir Lotfy/R_working_directory/UCI HAR Dataset/")
+
+#create variable names for the data frames 
 
 subject_train <- read.table("train/subject_train.txt")
 subject_test <- read.table("test/subject_test.txt")
@@ -6,3 +9,4 @@ X_train <- read.table("train/X_train.txt")
 X_test <- read.table("test/X_test.txt")
 y_train <- read.table("train/y_train.txt")
 y_test <- read.table("test/y_test.txt")
+featureNames <- read.table("features.txt")
